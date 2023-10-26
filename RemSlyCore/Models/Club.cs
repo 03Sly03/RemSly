adb1e8f4a7d85abe4f75bbe391692490ccf77138
Rémy Dumont <remy.dumont@labom2iformation.fr>
2023-10-26 10:03:56 +0200

    feat: Add-Models/Club

---
+++ RemSlyCore/Models/Club.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemSlyCore.Models
+{
+    public class Club
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        //public string Description { get; set; }
+        [Required]
+        public string? Address { get; set; }
+        [Required]
+        public string? PostalCode { get; set; }
+        [Required]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Invalid phone number")]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
+        [Required]
+        public string? OpeningHours { get; set; }
+        [Required]
+        public List<Session>? Sessions { get; set; }
+        [Required]
+        public List<Material>? Materials { get; set; }
+        [Required]
+        public bool IsLadiesOnly { get; set; }
+    }
+}
