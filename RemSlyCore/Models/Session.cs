779c7366dba4dba7ce860c9f59e7b661a8e2beab
Rémy Dumont <remy.dumont@labom2iformation.fr>
2023-10-26 10:24:01 +0200

    feat: Add-Models/Session

---
+++ RemSlyCore/Models/Session.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemSlyCore.Models
+{
+    public class Session
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        //public string? Description { get; set; }
+        [Required]
+        public string? Schedules { get; set; } // Possibilité de modifier en DateTime ?
+        [Required]
+        public int CapacityMax { get; set; }
+        [Required]
+        public bool IsComplete { get; set; }
+        
+        //public Dictionary<User> Notes {  get; set; }          // Voir pour la création
+        
+        public int Score { get; set; }
+        public List<Commentary>? Comments { get; set; }
+        public List<User>? Users { get; set; }
+    }
+}
