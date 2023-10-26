ba03e96b5e2ac58ce7ccd128da8e44adc6b9ee6d
Rémy Dumont <remy.dumont@labom2iformation.fr>
2023-10-26 09:58:12 +0200

    feat: Add-Models/User

---
+++ RemSlyCore/Models/User.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemSlyCore.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z].*", ErrorMessage = "FirstName must start with an Uppercase Letter !")]
+        public string? FirstName { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z].*", ErrorMessage = "LastName start with an Uppercase Letter !")]
+        public string? LastName { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        
+
+        [Required]
+        public string? Address { get; set; }
+
+        [Required]
+        public string PostalCode { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Invalid phone number")]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid email address")]
+        public string? Email { get; set; }
+        [Required]
+        //[PasswordValidator]   Voir selon délai si nous ajoutons le PasswordValidator
+        public string? PassWord { get; set; }
+        [Required]
+        public bool IsAdmin { get; set; } = false;
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+    }
+
+}
