ed3d563335294e164261093c47485c2e47523f1c
Sly <slydevwebproject@outlook.fr>
2023-10-29 00:03:25 +0200

    fix : wrong ImageUrl string

--- RemSlyCore/Datas/InitialSession.cs
+++ RemSlyCore/Datas/InitialSession.cs
@@ -13,23 +13,23 @@ namespace RemSlyCore.Datas
         {
             //new Session
             //{
-            //    Id = 1, Name = "Christophe", Schedules = "Chauveau", CapacityMax = Gender.Male, ImageUrl = "/images/avatar/Christophe.jpg" , Schedules = "7 rue de la Paix, 59000 Lille", IsComplete = "59000", Score = "christophe@gmail.com" , PassWord = "christophe123", PhoneNumber = "0607019001" , IsAdmin = true
+            //    Id = 1, Name = "Christophe", Schedules = "Chauveau", CapacityMax = Gender.Male, ImageUrl = "/Images/Avatar/Christophe.jpg" , Schedules = "7 rue de la Paix, 59000 Lille", IsComplete = "59000", Score = "christophe@gmail.com" , PassWord = "christophe123", PhoneNumber = "0607019001" , IsAdmin = true
             //},
             //new Session
             //{
-            //    Id = 2, Name = "Anthony", Schedules = "Boisselet", CapacityMax = Gender.Male, IsComplete = "/images/avatar/Anthony.jpg" , Address = "41 rue de Calais, 59130 Lambersart", PostalCode = "59130", Email = "anthony@gmail.com" , PassWord = "anthony123", PhoneNumber = "0647911071" , IsAdmin = false
+            //    Id = 2, Name = "Anthony", Schedules = "Boisselet", CapacityMax = Gender.Male, IsComplete = "/Images/Avatar/Anthony.jpg" , Address = "41 rue de Calais, 59130 Lambersart", PostalCode = "59130", Email = "anthony@gmail.com" , PassWord = "anthony123", PhoneNumber = "0647911071" , IsAdmin = false
             //},
             //new Session
             //{
-            //    Id = 3, Name = "Thomas", Schedules = "Lavaud", CapacityMax = Gender.Male, IsComplete = "/images/avatar/Thomas.jpg" , Address = "33 rue des Fleurs, 59650 Villeneuve d'Ascq", PostalCode = "59650", Email = "thomas@gmail.com" , PassWord = "thomas123", PhoneNumber = "0741810974" , IsAdmin = false
+            //    Id = 3, Name = "Thomas", Schedules = "Lavaud", CapacityMax = Gender.Male, IsComplete = "/Images/Avatar/Thomas.jpg" , Address = "33 rue des Fleurs, 59650 Villeneuve d'Ascq", PostalCode = "59650", Email = "thomas@gmail.com" , PassWord = "thomas123", PhoneNumber = "0741810974" , IsAdmin = false
             //},
             //new Session
             //{
-            //    Id = 4, Name = "Laura", Schedules = "Delcroix", CapacityMax = Gender.Female, IsComplete = "/images/trainer/LauraTrainer.png" , Address = "9 rue des plantes, 59200 Tourcoing", PostalCode = "59200", Email = "laura@gmail.com" , PassWord = "laura123", PhoneNumber = "0644810904" , IsAdmin = false
+            //    Id = 4, Name = "Laura", Schedules = "Delcroix", CapacityMax = Gender.Female, IsComplete = "/Images/Trainer/LauraTrainer.png" , Address = "9 rue des plantes, 59200 Tourcoing", PostalCode = "59200", Email = "laura@gmail.com" , PassWord = "laura123", PhoneNumber = "0644810904" , IsAdmin = false
             //},
             //new Session
             //{
-            //    Id = 5, Name = "Amelie", Schedules = "Duchemin", CapacityMax = Gender.Female, IsComplete = "/images/trainer/AmelieTrainer.jpg" , Address = "14 avenue de Dunkerque, 59160 Lille", PostalCode = "59160", Email = "amelie@gmail.com" , PassWord = "amelie123", PhoneNumber = "0709749425" , IsAdmin = false
+            //    Id = 5, Name = "Amelie", Schedules = "Duchemin", CapacityMax = Gender.Female, IsComplete = "/Images/Trainer/AmelieTrainer.jpg" , Address = "14 avenue de Dunkerque, 59160 Lille", PostalCode = "59160", Email = "amelie@gmail.com" , PassWord = "amelie123", PhoneNumber = "0709749425" , IsAdmin = false
             //},
 
         };
