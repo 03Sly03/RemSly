2e45e8eccc8f0bfa0ec721e8ac80e26d5b6b610a
Rémy Dumont <remy.dumont@labom2iformation.fr>
2023-10-31 09:42:33 +0100

    Update Repository ReservationSession

--- RemSlyApi/Repositories/ReservationSessionRepository.cs
+++ RemSlyApi/Repositories/ReservationSessionRepository.cs
@@ -18,54 +18,36 @@ namespace RemSlyApi.Repositories
 
             await _dbContext.SaveChangesAsync();
             return addedObj.Entity.SessionId > 0 && addedObj.Entity.ReservationId > 0;
-        //}
-        //public async Task<ReservationSession> GetById(int sessionid, int reservation id)
-        //{
-        //    return await _dbContext.ReservationSessions.FirstOrDefaultAsync(rs => (rsession.Id == id);
-        //}
+        }
+        public async Task<ReservationSession> GetById(int sessionid, int reservationid)
+        {
+            return await _dbContext.ReservationSessions.FirstOrDefaultAsync(rs => (rs.SessionId == sessionid && rs.ReservationId == reservationid));
+        }
 
-        //public async Task<ReservationSession?> Get(Expression<Func<ReservationSession, bool>> predicate)
-        //{
-        //    return await _dbContext.ReservationSessions.FirstOrDefaultAsync(predicate);
-        //}
+        public async Task<ReservationSession?> Get(Expression<Func<ReservationSession, bool>> predicate)
+        {
+            return await _dbContext.ReservationSessions.FirstOrDefaultAsync(predicate);
+        }
 
-        //public async Task<List<ReservationSession>> GetAll(Expression<Func<ReservationSession, bool>> predicate)
-        //{
-        //    return await _dbContext.ReservationSessions.Where(predicate).ToListAsync();
-        //}
+        public async Task<List<ReservationSession>> GetAll(Expression<Func<ReservationSession, bool>> predicate)
+        {
+            return await _dbContext.ReservationSessions.Where(predicate).ToListAsync();
+        }
 
-        //public async Task<List<ReservationSession>> GetAll()
-        //{
-        //    return await _dbContext.ReservationSessions.ToListAsync();
-        //}
+        public async Task<List<ReservationSession>> GetAll()
+        {
+            return await _dbContext.ReservationSessions.ToListAsync();
+        }
 
-        //public async Task<bool> Update(ReservationSession ReservationSession)
-        //{
-        //    var ReservationSessionFromDb = await GetById(ReservationSession.Id);
+        public async Task<bool> Delete(int sessionid, int reservationid)
+        {
+            var ReservationSessionToDelete = await GetById(sessionid, reservationid);
 
-        //    if (ReservationSessionFromDb == null)
-        //        return false;
+            if (ReservationSessionToDelete == null)
+                return false;
 
-        //    if (ReservationSessionFromDb.Name != ReservationSession.Name)
-        //        ReservationSessionFromDb.Name = ReservationSession.Name;
-        //    if (ReservationSessionFromDb.Schedules != ReservationSession.Schedules)
-        //        ReservationSessionFromDb.Schedules = ReservationSession.Schedules;
-        //    if (ReservationSessionFromDb.CapacityMax != ReservationSession.CapacityMax)
-        //        ReservationSessionFromDb.CapacityMax = ReservationSession.CapacityMax;
-
-
-        //    return await _dbContext.SaveChangesAsync() > 0;
-
-        //}
-        //public async Task<bool> Delete(int id)
-        //{
-        //    var ReservationSession = await GetById(id);
-
-        //    if (ReservationSession == null)
-        //        return false;
-
-        //    _dbContext.ReservationSessions.Remove(ReservationSession);
-        //    return await _dbContext.SaveChangesAsync() > 0;
-        //}
+            _dbContext.ReservationSessions.Remove(ReservationSessionToDelete);
+            return await _dbContext.SaveChangesAsync() > 0;
+        }
     }
 }
